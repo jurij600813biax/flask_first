4b550ec88614cc3ff2f9a2f6f1acff4af03906b5
jurij600813biax <jurij.tatur@gmail.com>
2020-04-15 22:32:41 +0300

    2020.04.25.22.35

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ create_login_schema = CreateLoginSchema()
 def get_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     if user is None:
-        return jsonify({"message": "user does not exist"}), 404
+       return jsonify({"message": "user does not exist"}), 404
     print(user.name)
     result = user_schema.dump(user)
     return jsonify({"data": result,"status": "202"}), 202
@@ -114,8 +114,3 @@ def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
-
-
-
-#if __name__ == '__main__':
-#    app.run (debug = True)
